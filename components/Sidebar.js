d73a8d8e05cc31c66cb61c9bc6bccd72aa96ef86
Kian Sam <itskiansam@gmail.com>
2021-08-05 20:37:08 +0300

    Nav and Sidebar

--- components/Sidebar.js
+++
@@ -1,9 +0,0 @@
-// import { useState } from "react";
-
-// function Sidebar() {
-//   const [sidebar, setSidebar] = useState(false);
-//    const showSidebar = () => setSidebar(!sidebar);
-//   return <div></div>;
-// }
-
-// export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
