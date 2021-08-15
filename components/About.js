cebb53ccf0f0174a7235a1f737a250676f1e8b9a
Kian Sam <itskiansam@gmail.com>
2021-08-15 17:16:57 +0300

    skills Animation

--- components/About.js
+++ components/About.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Slide from "react-reveal/Slide";
+import Fade from "react-reveal/Fade";
 
 function About() {
   return (
@@ -20,19 +22,29 @@ function About() {
             Kian Sam
           </span>
         </div>
-        <div className="flex px-8 sm:w-[480px] md:w-[560px] justify-center mx-auto mb-5">
-          <p>
-            I'm Kian, a Frontend web developer, and content creator. I've been
-            coding for over 3 years now. I'm currently working part-time as a
-            social media manager for a company. I know three languages, English,
-            Persian, and Turkish professionally, and I'm learning Dutch right
-            now.
+        <div className="px-8 sm:w-[568px] md:w-[748px] justify-center mx-auto mb-10">
+          <p className="sm:text-center">
+            I'm a Frontend web developer, Social Media Manager, and content
+            creator. I've been coding for over 3 years now. I'm currently
+            working part-time as a social media manager for a company. I know
+            three languages, English, Persian, and Turkish professionally, and
+            I'm learning Dutch right now.
           </p>
+          <div className="flex justify-center mx-auto mt-6">
+            <hr className="w-80 black" />
+          </div>
+        </div>
+        <div className="flex justify-start ml-8 sm:ml-0 sm:justify-center">
+          <div className="w-[60%] md:w-[44%] lg:w-[38%] h-[4px] bg-gray-300 z-10">
+            <Fade left>
+              <div className="w-[88%] h-[4px] bg-pink-500 z-0"></div>
+            </Fade>
+          </div>
         </div>
       </div>
-    </div>  
+    </div>
   );
-} 
+}
 
 export default About;
 
